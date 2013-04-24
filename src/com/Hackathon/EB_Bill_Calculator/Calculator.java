00617aa530be694784931839d70a843d7e11d8e6
Shobana <shobanam@thoughtworks.com>
2013-04-24 09:29:26 +0530

    Moved logic from web service to android with slab rates json as input.

--- src/com/Hackathon/EB_Bill_Calculator/Calculator.java
+++ src/com/Hackathon/EB_Bill_Calculator/Calculator.java
@@ -6,9 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Calculator extends Activity {
-
 
     private Button VWsubmit;
     private EditText VWfromUnits;
@@ -25,16 +27,135 @@ public class Calculator extends Activity {
         VWsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String billAmount = "";
-                String units = "";
-                Integer fromUnits =Integer.valueOf( VWfromUnits.getText().toString());
+                try {
+                //get view inputs
+                Integer fromUnits = Integer.valueOf(VWfromUnits.getText().toString());
                 Integer toUnits = Integer.valueOf(VWtoUnits.getText().toString());
 
+                //calculate bill
                 int totalUnits = toUnits - fromUnits;
-                String url = "http://tneb-billing.herokuapp.com/?units=" + totalUnits + "&mobile=1";
-                new calculateAsyncTask().execute(url,view.getContext());
+                JSONArray slabRatesJson = getSlabRatesJson();
+                float cost = getBill(slabRatesJson, totalUnits);
+
+                //display bill
+                Toast bill = Toast.makeText(view.getContext(), String.valueOf(cost), 1000);
+                bill.show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
     }
+
+    private JSONArray getSlabRatesJson() throws JSONException {
+
+        String slabRatesString = getSlabRatesString();
+        JSONArray slabRatesJson=new JSONArray(slabRatesString);
+        return slabRatesJson;
+    }
+
+    float getBill(JSONArray slabs, int units) throws JSONException {
+        float billAmount = 0;
+        float slabLimit = 0;
+        for(int i=0;i<slabs.length();i++){
+           JSONObject slab = (JSONObject)slabs.get(i);
+           boolean lastSlab = i == slabs.length() - 1;
+
+           if(!lastSlab){
+            slabLimit= getFloatValueForStringJson(slab, "slab");
+           }
+           if(lastSlab || units<slabLimit){
+                float fixedAmount = getFloatValueForStringJson(slab, "fixed");
+                JSONArray slots = slab.getJSONArray("slots");
+
+                billAmount = fixedAmount+ calculateSlabRate(slots, units);
+                return billAmount;
+           }
+        }
+        return billAmount;
+    }
+
+    private float getFloatValueForStringJson(JSONObject jsonObject, String stringJson) throws JSONException {
+        return Float.parseFloat(jsonObject.getString(stringJson));
+    }
+
+    private int calculateSlabRate(JSONArray slots, int units) throws JSONException {
+        int totalSlabAmount=0;
+        float lowerRange=0;
+       for(int i=0;i<slots.length();i++){
+           JSONObject slot = slots.getJSONObject(i);
+           float upperRange;
+           if(i==slots.length()-1){
+               upperRange=units;
+           }
+           else{
+           upperRange = getFloatValueForStringJson(slot, "range");
+           }
+           float slabRate = getFloatValueForStringJson(slot, "cost");
+           totalSlabAmount+=(upperRange-lowerRange)*slabRate;
+           lowerRange=upperRange;
+       }
+
+        return totalSlabAmount;
+    }
+
+    private String getSlabRatesString() {
+        String slabRates="[\n" +
+                "    {\n" +
+                "        \"slab\": \"100\",\n" +
+                "        \"fixed\": \"20\",\n" +
+                "        \"slots\": [\n" +
+                "            {\n" +
+                "                \"range\": \"100\",\n" +
+                "                \"cost\": \"1\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"slab\": \"200\",\n" +
+                "        \"fixed\": \"20\",\n" +
+                "        \"slots\": [\n" +
+                "            {\n" +
+                "                \"range\": \"200\",\n" +
+                "                \"cost\": \"1.5\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"slab\": \"500\",\n" +
+                "        \"fixed\": \"30\",\n" +
+                "        \"slots\": [\n" +
+                "            {\n" +
+                "                \"range\": \"200\",\n" +
+                "                \"cost\": \"2\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"range\": \"500\",\n" +
+                "                \"cost\": \"3\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"slab\": \"max\",\n" +
+                "        \"fixed\": \"40\",\n" +
+                "        \"slots\": [\n" +
+                "            {\n" +
+                "                \"range\": \"200\",\n" +
+                "                \"cost\": \"3\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"range\": \"500\",\n" +
+                "                \"cost\": \"4\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"range\": \"x\",\n" +
+                "                \"cost\": \"5.75\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "]";
+        return slabRates;
+    }
+
 }
+
